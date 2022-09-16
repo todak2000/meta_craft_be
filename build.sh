d335547140257f56f012be69c35c487fefb2bce2
Daniel Olagunju <todak2000@gmail.com>
2022-09-16 18:28:09 +0100

    signup tested

--- build.sh
+++ build.sh
@@ -8,3 +8,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --username daniel --password adminadmin --noinput --email 'daniel@email.com'
